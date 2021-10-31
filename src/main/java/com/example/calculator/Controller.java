5f36be05b01463326147821165d11cf5ef4ff01c
dev_ats <alextsiunel.work@gmail.com>
2021-10-31 17:21:38 +0300

    add class Controller and file calculator.fxml

---
+++ src/main/java/com/example/calculator/Controller.java
@@ -0,0 +1,113 @@
+package com.example.calculator;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button button_0;
+
+    @FXML
+    private Button button_1;
+
+    @FXML
+    private Button button_2;
+
+    @FXML
+    private Button button_3;
+
+    @FXML
+    private Button button_4;
+
+    @FXML
+    private Button button_5;
+
+    @FXML
+    private Button button_6;
+
+    @FXML
+    private Button button_7;
+
+    @FXML
+    private Button button_8;
+
+    @FXML
+    private Button button_9;
+
+    @FXML
+    private Button button_clean;
+
+    @FXML
+    private Button button_cleanentry;
+
+    @FXML
+    private Button button_division;
+
+    @FXML
+    private Button button_mclean;
+
+    @FXML
+    private Button button_mread;
+
+    @FXML
+    private Button button_msubtract;
+
+    @FXML
+    private Button button_msum;
+
+    @FXML
+    private Button button_multipl;
+
+    @FXML
+    private Button button_opposite;
+
+    @FXML
+    private Button button_point;
+
+    @FXML
+    private Button button_result;
+
+    @FXML
+    private Button button_subtract;
+
+    @FXML
+    private Button button_sum;
+
+    @FXML
+    void initialize() {
+        assert button_0 != null : "fx:id=\"button_0\" was not injected: check your FXML file 'calculator.fxml'.";
+        assert button_1 != null : "fx:id=\"button_1\" was not injected: check your FXML file 'calculator.fxml'.";
+        assert button_2 != null : "fx:id=\"button_2\" was not injected: check your FXML file 'calculator.fxml'.";
+        assert button_3 != null : "fx:id=\"button_3\" was not injected: check your FXML file 'calculator.fxml'.";
+        assert button_4 != null : "fx:id=\"button_4\" was not injected: check your FXML file 'calculator.fxml'.";
+        assert button_5 != null : "fx:id=\"button_5\" was not injected: check your FXML file 'calculator.fxml'.";
+        assert button_6 != null : "fx:id=\"button_6\" was not injected: check your FXML file 'calculator.fxml'.";
+        assert button_7 != null : "fx:id=\"button_7\" was not injected: check your FXML file 'calculator.fxml'.";
+        assert button_8 != null : "fx:id=\"button_8\" was not injected: check your FXML file 'calculator.fxml'.";
+        assert button_9 != null : "fx:id=\"button_9\" was not injected: check your FXML file 'calculator.fxml'.";
+        assert button_clean != null : "fx:id=\"button_clean\" was not injected: check your FXML file 'calculator.fxml'.";
+        assert button_cleanentry != null : "fx:id=\"button_cleanentry\" was not injected: check your FXML file 'calculator.fxml'.";
+        assert button_division != null : "fx:id=\"button_division\" was not injected: check your FXML file 'calculator.fxml'.";
+        assert button_mclean != null : "fx:id=\"button_mclean\" was not injected: check your FXML file 'calculator.fxml'.";
+        assert button_mread != null : "fx:id=\"button_mread\" was not injected: check your FXML file 'calculator.fxml'.";
+        assert button_msubtract != null : "fx:id=\"button_msubtract\" was not injected: check your FXML file 'calculator.fxml'.";
+        assert button_msum != null : "fx:id=\"button_msum\" was not injected: check your FXML file 'calculator.fxml'.";
+        assert button_multipl != null : "fx:id=\"button_multipl\" was not injected: check your FXML file 'calculator.fxml'.";
+        assert button_opposite != null : "fx:id=\"button_opposite\" was not injected: check your FXML file 'calculator.fxml'.";
+        assert button_point != null : "fx:id=\"button_point\" was not injected: check your FXML file 'calculator.fxml'.";
+        assert button_result != null : "fx:id=\"button_result\" was not injected: check your FXML file 'calculator.fxml'.";
+        assert button_subtract != null : "fx:id=\"button_subtract\" was not injected: check your FXML file 'calculator.fxml'.";
+        assert button_sum != null : "fx:id=\"button_sum\" was not injected: check your FXML file 'calculator.fxml'.";
+
+    }
+
+}
